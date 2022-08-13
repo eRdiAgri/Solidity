f0653ded7ca8eb3ab4fed74a1eea0a80f6cd62fa
eяdi.js <101503629+eRdiAgri@users.noreply.github.com>
2022-08-13 22:01:12 +0300

    Update solidity2.sol

---
+++ solidity2.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.7;
+
+contract Mapping {
+
+    mapping(address => bool) public _registered;
+    mapping(address => int256) public _amount;
+
+    function register(int256 amount) public {
+        //Detect registrations
+        require(!_registered[msg.sender], "Register error!");
+
+        _registered[msg.sender] = true;
+        _amount[msg.sender] = amount;
+    }
+
+
+    function Registered() public view returns(bool) {
+        return _registered[msg.sender];
+    }
+
+
+    function deleteRegistered() public {
+        require(Registered(), "user is not registered.");
+        delete(_registered[msg.sender]);
+        delete(_amount[msg.sender]);
+    }
+
+}
